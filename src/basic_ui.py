d7da9bf8698d00e3e39de60f5b999ed7c1cac837
Eric Lee <elee061202@gmail.com>
2026-09-23 23:31:14 -0500

    Added program starter to main.py, and added basic_ui.py.

---
+++ src/basic_ui.py
@@ -0,0 +1,58 @@
+import sys
+import time
+import fishing_events
+import trade_crates
+
+def exit_program():
+    print("Exiting the program. Goodbye!")
+    time.sleep(3)
+    sys.exit()
+
+def return_to_menu():
+    user_choice = input("Return to the main menu? (1: Yes/2: No): ")
+        
+    match user_choice:
+        case "1":
+            menu()
+        case "2":
+            exit_program()
+        case _:
+            print("Invalid input. Please enter '1' or '2'.")
+
+def menu():
+    program_running = True
+    
+    while program_running == True:
+        print("Select an option:")
+        print("1. Fishing Events")
+        print("2. Trade Crates")
+        print("3. Exit")
+        
+        user_choice = input("Enter your choice (1-3): ")
+        match user_choice:
+            case "1":
+                print("Fishing Events selected.")
+                precious = int(input("Enter the number of Precious Clams: "))
+                mystical = int(input("Enter the number of Mystical Clams: "))
+                bountiful = int(input("Enter the number of Bountiful Clams: "))
+                print()
+                
+                fishing_events.clams(precious, mystical, bountiful)
+                return_to_menu()
+            case "2":
+                print("Trade Crates selected.")
+                steel = int(input("Enter the number of Steel Ingot Crates: "))
+                bronze = int(input("Enter the number of Bronze Ingot Crates: "))
+                snowfield = int(input("Enter the number of Snowfield Jade Boxes: "))
+                calpheon = int(input("Enter the number of Calpheon Timber Crates: "))
+                serendia = int(input("Enter the number of Serendia Timber Crates: "))
+                thorn = int(input("Enter the number of Thorn Timber Crates: "))
+                palm = int(input("Enter the number of Palm Timber Crates: "))
+                print()
+                
+                trade_crates.valencia_to_nampo(steel, bronze, snowfield, calpheon, serendia, thorn, palm)
+                return_to_menu()
+            case "3":
+                exit_program()
+            case _:
+                print("Invalid choice. Please select a valid option (1-3).")
--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
+import sys
+import time
+import basic_ui
 import fishing_events
 import trade_crates
 
-# Fishing Profits (precious, mystical, bountiful)
-fishing_events.clams(8, 58, 121)
+# Program
+basic_ui.menu()
+
+# Hard-coded test cases for debugging purposes. Uncomment to test.
+# Fishing Profits (bountiful, mystical, precious)
+#fishing_events.clams(121, 58, 8)
 
 # Trade Crate Profits (steel, bronze, snowfield, calpheon, serendia, thorn, palm)
 #trade_crates.valencia_to_nampo(6196, 300, 90, 134, 750, 548, 1708)
